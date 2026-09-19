96d1541d671f955d4bc40f226ca80252a33a03d2
Gursharan-s <gursharansinghtech@gmail.com>
2026-09-20 00:22:00 +0530

    Refactor HTML structure and update CSS styles for layout and button design

--- dom.js
+++ dom.js
@@ -1,11 +1,11 @@
-var h = document.querySelector('h1');
+// var h = document.querySelector('h1');
 
-h.innerHTML = 'kidia bai'
+// h.innerHTML = 'kidia bai'
 
-h.style = 'pink'
-h.style.backgroundColor = 'yellow'
+// h.style = 'pink'
+// h.style.backgroundColor = 'yellow'
 
-h.addEventListener('click' ,function (){
-    console.log("oaaaa ");
+// h.addEventListener('click' ,function (){
+//     console.log("oaaaa ");
     
-})
+// })
