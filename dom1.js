a8ff3e3ba7d11dc7f612aeb09b60040658cfeada
Gursharan-s <gursharansinghtech@gmail.com>
2026-09-26 14:05:19 +0530

    Fix selector for progress line and improve code formatting

--- dom1.js
+++ dom1.js
@@ -1,15 +1,17 @@
 var btn = document.querySelector('button');
 var per = document.querySelector('.sec');
-var lin = document.querySelector('#line1');
+var lin = document.querySelector('.line1');
 var sta = document.querySelector('.fir');
 var grow = 0;
 
 btn.addEventListener('click', function () {
-    var int = setInterval(() => {
+    var int = setInterval(function() {
         grow++
         per.innerHTML = grow + '%'
-        lin.style.width = grow+'%'
+        lin.style.width = grow + '%'
     }, 50);
+
+
     setTimeout(function () {
         clearInterval(int)
         btn.innerHTML = 'Downloading'
